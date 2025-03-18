8fa3f3983ccbe261c7d1bfc25666d83d49e5c261
Pablo <pablopagliaricci@gmail.com>
2025-03-18 18:18:22 -0300

    some corrections

--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,11 @@
-use std::net::{TcpListener};
+use std::net::TcpListener;
 use std::thread;
 mod handler;
 
 pub fn run(address: &str) {
     let listener = TcpListener::bind(address).expect("No se pudo iniciar el servidor");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| {
-                    handler::handle_connection(stream);
-                });
-            }
-            Err(e) => eprintln!("Error al aceptar conexión: {}", e),
-        }
+    for stream in listener.incoming().flatten() {
+        thread::spawn(|| handler::handle_connection(stream));
     }
 }
--- src/server/handler.rs
+++ src/server/handler.rs
@@ -3,28 +3,17 @@ use std::net::TcpStream;
 mod math;
 
 pub fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    if let Ok(_) = stream.read(&mut buffer) {
-        let request = String::from_utf8_lossy(&buffer);
-
+    let mut request = String::new();
+    if stream.read_to_string(&mut request).is_ok() {
         if let Some(i) = parse_pi_request(&request) {
-            let result = math::calculate_pi(i);
-            let response = format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nValor de Pi para {}: {}\r\n",
-                i, result
-            );
-            stream.write_all(response.as_bytes()).unwrap();
+            let result = math::calculate_pi(i).to_string();
+            stream.write_all(result.as_bytes()).unwrap();
         } else {
-            let response = "HTTP/1.1 400 Bad Request\r\n\r\nFormato incorrecto\r\n";
-            stream.write_all(response.as_bytes()).unwrap();
+            stream.write_all(b"Error: Formato incorrecto").unwrap();
         }
     }
 }
 
 fn parse_pi_request(request: &str) -> Option<u64> {
-    if request.starts_with("GET /pi/") {
-        request.split_whitespace().nth(1)?.strip_prefix("/pi/")?.parse().ok()
-    } else {
-        None
-    }
+    request.strip_prefix("GET /pi/")?.split_whitespace().next()?.parse().ok()
 }
